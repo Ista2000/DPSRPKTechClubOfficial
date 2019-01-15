51f70c6fe4e426e331f07c4d625e32b15b37b059
ista2000 <mishra.istasis@gmail.com>
2019-01-16 04:39:58 +0530

    Generic Changes

--- pads/admin.py
+++ pads/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Pad
 
 # Register your models here.
+
+
+class PadAdmin(admin.ModelAdmin):
+
+    list_display = ['hash', ]
+
+
+admin.site.register(Pad, PadAdmin)
