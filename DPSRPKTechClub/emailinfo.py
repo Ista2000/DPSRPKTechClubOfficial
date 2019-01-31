ecf65ccd96a49600df6a396259c78437f0c5c111
Istasis Mishra <mishra.istasis@gmail.com>
2019-01-31 21:49:43 +0530

    Create emailinfo.py

---
+++ DPSRPKTechClub/emailinfo.py
@@ -0,0 +1,9 @@
+class Hide:
+    EMAIL_USE_TLS = True
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = 'mishra.istasis@gmail.com'
+    EMAIL_HOST_PASSWORD = 'yeetyeet'
+    EMAIL_PORT = 587
+    DATABASE_NAME = 'dpsrpktc'
+    DATABASE_USER = 'Magic Potato'
+    DATABASE_PASS = 'ista04072000'
