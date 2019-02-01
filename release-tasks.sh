e77e8f709d7dc30d9bb0fa59b82a576c216b93d6
ista2000 <mishra.istasis@gmail.com>
2019-02-01 16:55:07 +0530

    Modified procfile for migrations on release (heroku)

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,4 +1,3 @@
 #!/usr/bin bash
 python manage.py makemigrations
 python manage.py migrate
-echo "from users.models import CustomUser; CustomUser.objects.create_superuser('admin', 'admin@example.com', 'admin123')" | python manage.py shell
