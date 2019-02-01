bfbb1ec6caf409bd58d55c88de0232604cc86d21
ista2000 <mishra.istasis@gmail.com>
2019-02-01 16:46:47 +0530

    Modified procfile for migrations on release (heroku)

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,3 +1,4 @@
 #!/usr/bin bash
 python3 manage.py makemigrations
 python3 manage.py migrate
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin123')" | python manage.py shell
